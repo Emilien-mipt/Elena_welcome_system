ae07b575b95a9fd47320d9e65de25625328c45ca
Emin <emin.tagiev@phystech.edu>
2020-01-15 17:26:56 +0300

    Added automatic processing of images to gain encodings, automatic playing of videos corresponding to recognised people + added ratio to resize frames

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import cv2
 import face_recognition
 import numpy as np
 
-
 """
 This is a demo of running face recognition on live video from your webcam.
 It's a little more complicated than the other example,
@@ -19,66 +18,40 @@ It's only required if you want to run this specific demo.
 If you have trouble installing it, try any of the other demos that don't require it instead.
 """
 
+PIC_PATH = "./pics/Robotics_Lab/"
+VID_PATH = "./videos/Robotics_Lab/"
+RATIO = 2.0  # Ratio by which the frame is resized to make the program work faster
 
-def play_video(path):
-    """Play welcoming video."""
-    # Create a VideoCapture object and read from input file
-    cap = cv2.VideoCapture(path)
-
-    # Check if camera opened successfully
-    if cap.isOpened() is False:
-        print("Error opening video  file")
-
-    # Read until video is completed
-    while cap.isOpened():
-
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret:
-            # Display the resulting frame
-            cv2.imshow("Frame", frame)
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord("q"):
-                break
-        # Break the loop
-        else:
-            break
-    # When everything done, release
-    # the video capture object
-    cap.release()
-    # Closes all the frames
-    cv2.destroyAllWindows()
-    # When everything done, release
-    # the video capture object
-    cap.release()
-    # Closes all the frames
-    cv2.destroyAllWindows()
-
-
-real_sim = True
+REAL_SIM = True
 
 # Get a reference to cameras
-if real_sim:
+if REAL_SIM:
     video_capture = cv2.VideoCapture(2)  # RealSim camera
 else:
     video_capture = cv2.VideoCapture(0)  # Default webcam
 
+image_names = os.listdir(PIC_PATH)
+image_names.sort()
+N = len(image_names)  # Number of people
+
+video_names = os.listdir(VID_PATH)
+video_names.sort()
+
+known_face_encodings = []
+known_face_names = [0 for i in range(N)]
+
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file(
-    os.path.abspath("pics/examples/obama.jpg")
-)
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+for i in range(N):
+    path_to_image = os.path.join(PIC_PATH, image_names[i])
+    image = face_recognition.load_image_file(path_to_image)
+    known_face_encodings.append(face_recognition.face_encodings(image)[0])
+    name = os.path.basename(path_to_image)
+    known_face_names[i] = os.path.splitext(name)[0]
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file(
-    os.path.abspath("pics/examples/biden.jpg")
-)
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-# Create arrays of known face encodings and their names
-known_face_encodings = [obama_face_encoding, biden_face_encoding]
-known_face_names = ["Barack Obama", "Joe Biden"]
-known_face_names_flags = [False, False]
+print("Names: ", known_face_names)
+print("known face encodings: ", len(known_face_encodings))
+# Create arrays to remember the faces, that were already recognised by the camera
+known_face_names_flags = [False for i in range(N)]
 
 # Initialize some variables
 face_locations = []
@@ -91,7 +64,7 @@ while True:
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=1.0 / RATIO, fy=1 / RATIO)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -131,7 +104,7 @@ while True:
                         [
                             "/usr/bin/ffplay",
                             "-autoexit",
-                            os.path.abspath("videos/examples/obama.mp4"),
+                            os.path.join(VID_PATH, name + ".mp4"),
                         ]
                     )
                     known_face_names_flags[best_match_index] = True
@@ -141,10 +114,10 @@ while True:
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top *= int(RATIO)
+        right *= int(RATIO)
+        bottom *= int(RATIO)
+        left *= int(RATIO)
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
